99a2992208bd3e1c52587684c25762e9e99dc30a
VladimirPolozov <57739258+VladimirPolozov@users.noreply.github.com>
2023-10-18 15:32:56 +0700

    ADDED post-condition into CYCLE-with-conditions.cpp

--- CYCLE-with-conditions.cpp
+++ CYCLE-with-conditions.cpp
@@ -1,7 +1,7 @@
 /***************************************
 * Author: Владимир Полозов             *
 * Name: Циклы с пред- и постусловием   *
-* URL: https://onlinegdb.com/QVIbddiQA *
+* URL: https://onlinegdb.com/mBh-qhedC *
 ****************************************/
 #include <iostream>
 #include <iomanip>
@@ -9,7 +9,8 @@
 using namespace std;
 
 int main() {
-  float T, V1, V2[9], A1[9], A2[9];
+  float T, V1, A1[9], A2[9];
+  float V2 = 2;
   const float R = 8.31; // газовая постоянная кДж/град
   const float n = 1.2;  // показатель адиабаты
   
@@ -18,23 +19,22 @@ int main() {
   cout << "Начальный объём газа (в м3) = ";
   cin >> V1; // 2
   cout << "Конечный объём газа (в м3, 9 значений отделенных переносом строки):\n";
-  int input_counter = 0;
-  while (input_counter < 9) {
-      cin >> V2[input_counter]; // 2.2, 2.4, 2.6, 2.8, 3, 4, 5, 6, 7}
-      input_counter += 1;
-  }
-  
+  // 2.2, 2.4, 2.6, 2.8, 3, 4, 5, 6, 7}
+
   cout << fixed;
   cout << setprecision(3);
   
-  int index_of_array_element = 0;
-  while (index_of_array_element < 9) {
-    A1[index_of_array_element] = R * T * log( ( V2[index_of_array_element] / V1 ) );
-    A2[index_of_array_element] = R * T / (n - 1) * ( 1 - pow( ( V1 / V2[index_of_array_element] ), (n - 1) ) );
-    cout << "V2 = " << V2[index_of_array_element]\
-         << "\tA1 = " << A1[index_of_array_element]\
-         << "\tA2 = " << A2[index_of_array_element] << '\n';
-    index_of_array_element += 1;
+  do {
+    V2 += 0.2;
+    cout << "V2 = " << V2\
+         << "\tA1 = " << R * T * log( ( V2 / V1 ) )\
+         << "\tA2 = " << R * T / (n - 1) * ( 1 - pow( ( V1 / V2 ), (n - 1) ) ) << endl;
+  } while (V2 < 3);
+  while (V2 < 7) {
+    V2 += 1;
+    cout << "V2 = " << V2\
+         << "\tA1 = " << R * T * log( ( V2 / V1 ) )\
+         << "\tA2 = " << R * T / (n - 1) * ( 1 - pow( ( V1 / V2 ), (n - 1) ) ) << endl;
   }
   
   return 0;
